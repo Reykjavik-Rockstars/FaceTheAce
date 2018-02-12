526e5f6d756e16a61c86910305dcf2849419e533
Tyler G Hudson <tgh22@txstate.edu>
2018-02-12 00:55:14 -0600

    Client Login Screen
    Implemented the beginning GUI code for the client login screen, containing code for accepting and storing an IP Address and a username in a static GameObject.

---
+++ RockstarGame_Client/Assets/ClientInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using UnityEngine;
+
+interface IUserName
+{
+    string userName{get; set;}
+}
+
+public class ClientInfo : MonoBehaviour {
+
+    public static ClientInfo singleton;
+
+    // The below regular expression is capable of checking for a non-empty string.
+    static string unRegex = "\\b.+\\b";
+
+    string _username;
+    public string userName
+    {
+        get { return _username; }
+        set { _username = value; }
+    }
+
+	void Awake () {
+        singleton = this;
+	}
+
+    public bool IsValid()
+    {
+        if (_username == null) { return false; }
+        Regex rx = new Regex(unRegex);
+        return rx.IsMatch(_username);
+    }
+}
