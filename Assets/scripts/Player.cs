93f09e70fe583f826f0d96d57fedb293774ca486
Tyler G Hudson <tgh22@txstate.edu>
2018-04-08 19:55:37 -0500

    Wired in new scenes

--- Assets/scripts/Player.cs
+++ Assets/scripts/Player.cs
@@ -1,29 +1,59 @@
 ﻿using UnityEngine;
-
-public class Player
+public class Player : MonoBehaviour
 {
     public string Username;
     public int Health;
     public Hand Hand;
-
+    //on start, player is not dead. Only dead once hp falls to 0 or less
+    public bool isDead = false;
     public Del_Int OnDamaged;
     public void ReceiveDamage(int damage, Player source)
     {
         if (OnDamaged != null) OnDamaged(damage);
+        //only receive damage if player is alive
+        if (!isDead)
+        {
+            //if damage is less than player health, deal normal damage
+            if (Health > 0 && Health >= damage)
+                Health = Health - damage;
+            //if health is less than or equal to damage, health goes to 0 and player dies
+            else if (Health <= damage)
+            {
+                Health = 0;
+                Die();
+            }
+            //make sure player health is 0 for other conditions. prevent negative health.
+            else
+                Health = 0;
+        }
     }
-
     public Del_Int OnHealed;
     public void ReceiveHeal(int heal, Player source)
     {
         if (OnHealed != null) OnHealed(heal);
-    }
 
+        //only heal if player is alive
+        if (!isDead)
+        {
+            //add health normally if health is normal
+            if (Health >= 0)
+            {
+                Health = Health + heal;
+            }
+            //if health is below 0 for some reason, reset health to 0 and heal normally
+            else
+            {
+                Health = 0;
+                Health = Health + heal;
+            }
+        }
+    }
     public Del_Void OnDeath;
     void Die()
     {
         if (OnDeath != null) OnDeath();
+        isDead = true;
     }
-
     public Del_ListCard OnCardDraw;
     public Del_Card OnSelect;
     public Del_Player_Card OnTarget;
