613db242b81fc9c2988cb53d7bb9081f0135732b
dac183 <dac183@txstate.edu>
2018-04-20 23:12:11 -0500

    Merged Lobby Data with main scene
    See ActivePlayer.cs & OnLobbyServerSceneLoadedForPlayer() in LobbyManager.cs for main changes

--- Assets/scripts/delegates.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Collections.Generic;
-
-public delegate void Del_Void();
-public delegate void Del_Int(int x);
-public delegate void Del_Card(Card c);
-public delegate void Del_ListCard(List<Card> cards);
-public delegate void Del_Player_Card(Player p, Card c);
